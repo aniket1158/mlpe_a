fbe81efcee7b809b4455b26e6db6ba1d8a1986cd
Aniket Jamkar <aniketjamkar11@gmail.com>
2023-12-30 19:59:15 +0530

    data ingestion

---
+++ src/components/data_transformation.py
@@ -0,0 +1,13 @@
+import sys
+from dataclasses import dataclass
+import os
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
